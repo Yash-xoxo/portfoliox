b7ec4e99cbe3767c1f2e409e1accf9b4c5fed8dc
Yash <satorufxd@gmail.com>
2025-07-09 23:43:53 +0530

    runn

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfoliox/', // 👈 Required for GitHub Pages deployment
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
 });
